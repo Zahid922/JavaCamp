df81bbb0c6d203c82253f7df7ad6203bb174349e
Zahid922 <83453923+Zahid922@users.noreply.github.com>
2021-05-05 19:12:16 +0300

    Add files via upload

---
+++ Day4_3/src/Concrete/GamerCheckManager.java
@@ -0,0 +1,14 @@
+package Concrete;
+
+import Abstracts.CheckService;
+import Entities.Gamer;
+
+public class GamerCheckManager implements CheckService{
+
+    public boolean GamerIfRealPerson(Gamer gamer) {
+       
+        
+        return true;
+    }
+
+}
